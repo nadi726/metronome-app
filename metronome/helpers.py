a5b8ee0d593483106f31e28e2c9182baae2f4bd3
nadi726 <nadavlevi726@gmail.com>
2021-09-22 12:20:39 +0300

    autoformatted python files with black

--- metronome/helpers.py
+++ metronome/helpers.py
@@ -7,47 +7,50 @@ from kivy.utils import platform
 class Spotify(spotipy.Spotify):
     def __init__(self):
         config = configparser.ConfigParser()
-        config.read('metronome/config.cfg')
-        client_id = config.get('SPOTIFY', 'CLIENT_ID')
-        client_secret = config.get('SPOTIFY', 'CLIENT_SECRET')
-        
-        super().__init__(auth_manager=SpotifyClientCredentials(
-            client_id=client_id,
-            client_secret=client_secret
-        ))
-    
+        config.read("metronome/config.cfg")
+        client_id = config.get("SPOTIFY", "CLIENT_ID")
+        client_secret = config.get("SPOTIFY", "CLIENT_SECRET")
+
+        super().__init__(
+            auth_manager=SpotifyClientCredentials(
+                client_id=client_id, client_secret=client_secret
+            )
+        )
+
     def get_song_metadata(self, song, artist):
         # Get song id
         # TODO: make a filtering algorithm for song and artist name
-        results = self.search(q=f'track:"{song}" artist:"{artist}"', type='track', limit=5)["tracks"]["items"]
+        results = self.search(
+            q=f'track:"{song}" artist:"{artist}"', type="track", limit=5
+        )["tracks"]["items"]
         if not results:
             return None
 
         song_id = results[0]["id"]
         song = self.track(song_id)
-        
+
         # Get Relevant metadata
         artists = [artist["name"] for artist in song["artists"]]
-        song_metadata = {"name" : song["name"], "artists" :artists}
-        
+        song_metadata = {"name": song["name"], "artists": artists}
+
         audio_analysis = self._get_relevant_audio_analysis(song_id)
 
         # Merge song and artists names with the rest of the metadata
         song_metadata = {**song_metadata, **audio_analysis}
         self._format_metadata(song_metadata)
         return song_metadata
-    
+
     def _get_relevant_audio_analysis(self, song_id):
         """extract relevant info from audio analysis and format it"""
         full_analysis = self.audio_analysis(song_id)["track"]
         fields = ("tempo", "time_signature", "duration", "mode", "key")
-        analysis = {i : full_analysis[i] for i in fields}
+        analysis = {i: full_analysis[i] for i in fields}
         return analysis
-    
+
     def _format_metadata(self, metadata):
         # Convert duration to minutes:seconds
         m, s = divmod(int(metadata["duration"]), 60)
-        metadata["duration"] = f'{m:02d}:{s:02d}'
+        metadata["duration"] = f"{m:02d}:{s:02d}"
 
         # convert key to letters
         keys = "C C#/Db D D#/Eb E F F#/Gb G G#/Ab A A#/Bb B".split()
@@ -64,19 +67,21 @@ class PlaySound:
         # Use pygame's mixer on platforms other than android
         if platform != "android":
             from pygame import mixer
+
             mixer.pre_init(44100, -16, 2, 256)
             mixer.init()
             self.sound = mixer.Sound("metronome/resources/click.wav")
-        
+
         # On android use pyjnius
         # Solution by Jonathan De: stackoverflow.com/questions/56325453/kivy-play-sound-on-android-without-delay
         else:
             from jnius import autoclass
-            MediaPlayer = autoclass('android.media.MediaPlayer')
+
+            MediaPlayer = autoclass("android.media.MediaPlayer")
             self.sound = mPlayer_norm = MediaPlayer()
             mPlayer_norm.setDataSource(soundfile)
             mPlayer_norm.prepare()
-    
+
     def play(self):
         self.sound.play()
 
--- metronome/metronome.py
+++ metronome/metronome.py
@@ -1,4 +1,5 @@
 from kivy.config import Config
+
 Config.set("kivy", "kivy_clock", "free_all")
 
 from kivymd.app import MDApp
@@ -12,23 +13,25 @@ from kivy.core.window import Window
 from kivymd.uix.button import MDIconButton
 
 from metronome.helpers import PlaySound, Spotify
+
 """<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>"""
 
-#Builder.load_file("metronome.kv")
+# Builder.load_file("metronome.kv")
 Window.size = (450, 600)
 
 
 class PlayButton(MDIconButton):
     bpm = NumericProperty(None)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
+
         self.icon = "resources/play-button.png"
         self.sound = PlaySound("click.wav")
-        self.event = None # For clock event
+        self.event = None  # For clock event
         self.is_playing = False
-        self.bind(bpm = self.on_bpm_change)
-    
+        self.bind(bpm=self.on_bpm_change)
+
     def on_press(self):
         if not self.is_playing:
             self.play()
@@ -39,7 +42,7 @@ class PlayButton(MDIconButton):
         self.icon = "resources/pause-button.png"
         self.event = Clock.schedule_interval_free(self.play_sound, 60 / self.bpm)
         self.is_playing = True
-    
+
     def stop(self):
         self.icon = "resources/play-button.png"
         self.event.cancel()
@@ -50,22 +53,23 @@ class PlayButton(MDIconButton):
             # Reset and play
             self.event.cancel()
             self.play()
-        
+
     def play_sound(self, dt):
         self.sound.play()
 
-    
+
 class MainLayout(FloatLayout):
     sp = Spotify()
     song_name = ObjectProperty(None)
     bpm = NumericProperty(120)
     max_bpm = 240
     min_bpm = 20
-    metadata = dict.fromkeys(["name", "artists", "tempo",
-                     "time_signature", "duration", "mode", "key"], '')
-    
+    metadata = dict.fromkeys(
+        ["name", "artists", "tempo", "time_signature", "duration", "mode", "key"], ""
+    )
+
     def on_search(self):
-        """ Is binded to the search button's on_press method
+        """Is binded to the search button's on_press method
         Gets metadata based on the text input and returns results accordingly.
         """
         search_text = self.song_name.text
@@ -78,17 +82,16 @@ class MainLayout(FloatLayout):
         metadata = self.sp.get_song_metadata(song, artist)
         if metadata is None:
             return
-        
+
         self.bpm = round(metadata["tempo"])
         self.metadata = metadata
         print("Song metadata:\n" + str(self.metadata))
-         
- 
-class MetronomeApp(MDApp):
 
+
+class MetronomeApp(MDApp):
     def build(self):
         return MainLayout()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MetronomeApp().run()
